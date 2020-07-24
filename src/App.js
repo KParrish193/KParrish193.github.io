8b94a8af54ef0f086e0a52c291ca388f4e0d69ef
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-07-24 11:11:41 -0600

    updated paths again

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
                 </Link>
 
                 <NavLink 
-                    to="/kp/projects" 
+                    to="/projects" 
                     className="left"
                     activeStyle={{
                         fontWeight: "bold",
@@ -36,7 +36,7 @@ function App() {
                 </Link>  
 
                 <NavLink 
-                    to="/kp/skills" 
+                    to="/skills" 
                     className="right"
                     activeStyle={{
                         fontWeight: "bold",
@@ -49,9 +49,9 @@ function App() {
         </header>
 
     <div>
-        <Route exact path="/kp" component={About} />
-        <Route path="/kp/projects" component={Projects} />
-        <Route path="/kp/skills" component={Skills} />
+        <Route exact path="/" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/skills" component={Skills} />
     </div>
     </AppContainer>
 );
