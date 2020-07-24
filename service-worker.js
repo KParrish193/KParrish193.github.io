be8b4531fc503dc66161dc19503c9b2d030f6cfe
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-07-24 11:00:23 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe60935b3507d18d247b39073760cbeb.js"
+  "/precache-manifest.6fdfd76b4025f4e4cc8d0e57eedcd8bf.js"
 );
 
 self.addEventListener('message', (event) => {
